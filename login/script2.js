81a151e09a4bbd2f3b5787cdbf82440d1431cfaf
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-10-02 02:07:42 +0000

    Oct 2, 2024, 9:07 AM

--- login/script2.js
+++ login/script2.js
@@ -1,16 +1,3 @@
-function togglePassword1() {
-  var passwordInput = document.getElementById("password");
-  var toggleButton = document.querySelector(".toggle-password");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    toggleButton.textContent = "🙈";
-  } else {
-    passwordInput.type = "password";
-    toggleButton.textContent = "👁️";
-  }
-  
-}
-
 function togglePassword2() {
   var passwordInput = document.getElementById("authenticated");
   var toggleButton = document.querySelector(".toggle-password2");
