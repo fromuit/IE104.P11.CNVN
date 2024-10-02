193387d2e267922dffacf367ea1dbbadb410357a
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-10-02 02:20:32 +0000

    Oct 2, 2024, 9:20 AM

--- login/script.js
+++ login/script.js
@@ -1,6 +1,6 @@
 function togglePassword1() {
   var passwordInput = document.getElementById("password");
-  var toggleButton = document.querySelector(".toggle-password");
+  var toggleButton = document.querySelector(".toggle-password1");
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
     toggleButton.textContent = "🙈";
