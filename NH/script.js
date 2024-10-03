84e363732329db0936c103a60959afb9c490c00b
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-10-03 02:17:33 +0000

    Oct 3, 2024, 9:17 AM

--- NH/script.js
+++ NH/script.js
@@ -1,11 +1,19 @@
-
+function email_check(email){
+  const email_pattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return email_pattern.test(email);
+}
 
 
 var signup=function(){
   let password=document.querySelector(".password").value;
   let confirmPassword=document.querySelector(".confirm-password").value;
-  if(password==confirmPassword){
+  let email_check=document.querySelector(".email").value;
+  if(!email_check){
+    window.alert("Email is not valid!");
+  }
+  else if(password==confirmPassword){
     window.alert("Sign up successful!");
+    return;
   }
   else{
     window.alert("Password and confirm password do not match!");
