726600c0341f2917024fe4613023a6edca9b1481
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-10-29 01:54:07 +0000

    Oct 29, 2024, 8:54 AM

--- Hải Trần's folder/reactjs + nodejs/src/main.jsx
+++ Hải Trần's folder/reactjs + nodejs/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from "D:\\IE104.P11.CNVN\\Hải Trần's folder\\reactjs + nodejs\\src\\web_truyen.jsx"
+import Header from "D:/IE104.P11.CNVN/Hải Trần's folder/reactjs + nodejs/src/web_truyen.jsx"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
