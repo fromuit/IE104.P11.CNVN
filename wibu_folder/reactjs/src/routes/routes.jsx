cf93c843cbe06b52af0456f116bff0d4593781a6
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-11-01 19:39:20 +0700

    Refactor routing in App component and remove unused files

--- wibu_folder/reactjs/src/routes/routes.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom';
-import MainPage from "../pages/main_page.jsx";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage />,
-  },
-]);
