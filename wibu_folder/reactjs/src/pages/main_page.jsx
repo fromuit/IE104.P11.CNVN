f269942d1649fac6e21fe3a95cfd37f5fef07497
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-11-01 11:57:49 +0000

    Nov 1, 2024, 6:57 PM

--- wibu_folder/reactjs/src/pages/main_page.jsx
+++ wibu_folder/reactjs/src/pages/main_page.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from './components/Main-page/Header/Header';
 import Main from './components/Main-page/Main/Main';
 import Footer from './components/Main-page/Footer/Footer';
