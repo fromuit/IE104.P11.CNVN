bc77c9100d8c0c04ae153ed9856d1cbd3d5540c2
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-11-01 09:50:47 +0700

    Add new user to users.json and update AuthPage instructions

--- login/login_of_tris/src/components/main_page/AuthPage.jsx
+++ login/login_of_tris/src/components/main_page/AuthPage.jsx
@@ -6,6 +6,11 @@ export const AuthPage = () => {
     <div>
       <div className='auth-container'>
        
+       <p>Trước khi chạy server, cần chạy lệnh: npm install express fs path cors
+        <br/>Chuyển tới thư mục server
+        <br/>Sau đó chạy lệnh: node server.js
+        <br/>Chuyển về thư mục src
+        <br/>Sau đó ms chạy vite bt</p>
         <Signup />
        
         <Login />
