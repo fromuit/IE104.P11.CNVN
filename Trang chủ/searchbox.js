79bee151918417297814c406cb562b36ffe9cbda
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-11-02 20:00:37 +0700

    Refactor file structure and remove unnecessary files

--- Trang chủ/searchbox.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.search-bar').addEventListener('click', function() {
-  var searchText = document.querySelector('.search-bar input[type="text"]').value;
-  // Thực hiện tìm kiếm
-});
