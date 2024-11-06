3b01b0b2b1e2c4e8c7a3701abff4ce4a91ea2f0f
EldlichLord2004 <22521522@gm.uit.edu.vn>
2024-11-06 05:54:03 +0000

    Nov 6, 2024, 12:54 PM

--- web_wibu_react/src/components/Header/BottomNav/BottomNav.jsx
+++ web_wibu_react/src/components/Header/BottomNav/BottomNav.jsx
@@ -77,72 +77,67 @@ function BottomNav() {
       <div 
         className={`bottom-nav__wrapper ${!isHomePage ? 'bottom-nav__wrapper--compact' : ''}`} 
         ref={wrapperRef}
-      ></div>
-      <nav 
-        className={`bottom-nav ${!isHomePage ? 'bottom-nav--compact' : ''}`} 
-        ref={navRef}
       >
-        <div className="bottom-nav__container">
-          <ul className="bottom-nav__list">
-            <li className="bottom-nav__item" ref={genresRef}>
-              <div 
-                className="bottom-nav__link"
-                onClick={() => setShowGenres(!showGenres)}
-                style={{ cursor: 'pointer' }}
-              >
-                <i className="fas fa-tags"></i>
-                Thể loại
-                <i className={`fas fa-chevron-${showGenres ? 'up' : 'down'} ml-1`}></i>
-              </div>
-              <div className={`genres-dropdown ${showGenres ? 'show' : ''}`}>
-                <ul className="genres-list">
-                  {genresData.genres.map(genre => (
-                    <li key={genre.id}>
-                      <NavLink 
-                        to={`/the-loai/${genre.slug}`}
-                        className="genre-item"
-                        onClick={() => setShowGenres(false)}
-                      >
-                        {genre.name}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-            <li className="bottom-nav__item">
-              <NavLink to="/lich-phat-hanh" className="bottom-nav__link">
-                <i className="far fa-calendar-alt"></i>
-                Lịch phát hành
-              </NavLink>
-            </li>
-            <li className="bottom-nav__item">
-              <NavLink to="/dang-truyen" className="bottom-nav__link">
-                <i className="fas fa-upload"></i>
-                Đăng truyện
-              </NavLink>
-            </li>
-            <li className="bottom-nav__item">
-              <NavLink to="/cua-hang" className="bottom-nav__link">
-                <i className="fas fa-store"></i>
-                Cửa hàng
-              </NavLink>
-            </li>
-            <li className="bottom-nav__item">
-              <NavLink to="/bang-xep-hang" className="bottom-nav__link">
-                <i className="fas fa-trophy"></i>
-                Bảng xếp hạng
-              </NavLink>
-            </li>
-            <li className="bottom-nav__item">
-              <NavLink to="/quy-dinh" className="bottom-nav__link">
-                <i className="fas fa-book"></i>
-                Quy định
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav 
+          className={`bottom-nav ${!isHomePage ? 'bottom-nav--compact' : ''}`} 
+          ref={navRef}
+        >
+          <div className="bottom-nav__container">
+            <ul className="bottom-nav__list">
+              <li className="bottom-nav__item" key="genres">
+                <div 
+                  className="bottom-nav__link"
+                  onClick={() => setShowGenres(!showGenres)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  <i className="fas fa-tags"></i>
+                  Thể loại
+                  <i className={`fas fa-chevron-${showGenres ? 'up' : 'down'} ml-1`}></i>
+                </div>
+                <div className={`genres-dropdown ${showGenres ? 'show' : ''}`}>
+                  <ul className="genres-list">
+                    {genresData.genres.map((genre, index) => (
+                      <li key={`genre-${genre.id}-${index}`}>
+                        <NavLink 
+                          to={`/the-loai/${genre.slug}`}
+                          className="genre-item"
+                          onClick={() => setShowGenres(false)}
+                        >
+                          {genre.name}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+              <li className="bottom-nav__item" key="rank">
+                <NavLink to="/bang-xep-hang" className="bottom-nav__link">
+                  <i className="fas fa-chart-line"></i>
+                  Bảng xếp hạng
+                </NavLink>
+              </li>
+              <li className="bottom-nav__item" key="follow">
+                <NavLink to="/theo-doi" className="bottom-nav__link">
+                  <i className="fas fa-heart"></i>
+                  Theo dõi
+                </NavLink>
+              </li>
+              <li className="bottom-nav__item" key="history">
+                <NavLink to="/lich-su" className="bottom-nav__link">
+                  <i className="fas fa-history"></i>
+                  Lịch sử
+                </NavLink>
+              </li>
+              <li className="bottom-nav__item" key="account">
+                <NavLink to="/tai-khoan" className="bottom-nav__link">
+                  <i className="fas fa-user"></i>
+                  Tài khoản
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </>
   );
 }
