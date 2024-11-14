d4a4a81e66ab0467a61654636170f4ed3f4d25c6
Nguyễn Hoàng Duy <22520327@gm.uit.edu.vn>
2024-11-14 21:54:28 +0700

    Features Pagination, apply SCSS + Module to Genres page

--- web_wibu_react/src/features/Advanced-Search/AdvancedSearch.jsx
+++ web_wibu_react/src/features/Advanced-Search/AdvancedSearch.jsx
@@ -3,104 +3,105 @@ import { Link, useSearchParams } from 'react-router-dom';
 import TopNav from '../../components/Header/TopNav/TopNav';
 import Banner from '../../components/Header/Banner/Banner';
 import { searchNovels } from '../utils/searchUtils';
-import genreMapping from '../../data_and_source/Novel_Data/gerne_mapping.json';
-import './AdvancedSearch.css';
+import genreMapping from '../../data_and_source/Novel_Data/genre_mapping.json';
+// import './AdvancedSearch.css';
+import Pagination from '../Pagination/Pagination';
 import styles from './AdvancedSearch.module.scss';
-import PropTypes from 'prop-types';
-import Novel_Data from '../../data_and_source/Novel_Data/hako_data.json';
+// import PropTypes from 'prop-types';
+// import Novel_Data from '../../data_and_source/Novel_Data/hako_data.json';
 
-const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-  Pagination.propTypes = {
-    currentPage: PropTypes.number.isRequired,
-    totalPages: PropTypes.number.isRequired, 
-    onPageChange: PropTypes.func.isRequired
-  };
+// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+//   Pagination.propTypes = {
+//     currentPage: PropTypes.number.isRequired,
+//     totalPages: PropTypes.number.isRequired, 
+//     onPageChange: PropTypes.func.isRequired
+//   };
 
-  const renderPaginationButtons = () => {
-    const pages = [];
-    const maxVisiblePages = 5;
-    let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
-    let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
+//   const renderPaginationButtons = () => {
+//     const pages = [];
+//     const maxVisiblePages = 5;
+//     let startPage = Math.max(1, currentPage - Math.floor(maxVisiblePages / 2));
+//     let endPage = Math.min(totalPages, startPage + maxVisiblePages - 1);
     
-    if (endPage - startPage + 1 < maxVisiblePages) {
-      startPage = Math.max(1, endPage - maxVisiblePages + 1);
-    }
+//     if (endPage - startPage + 1 < maxVisiblePages) {
+//       startPage = Math.max(1, endPage - maxVisiblePages + 1);
+//     }
 
-    pages.push(
-      <button 
-        key="first" 
-        onClick={() => onPageChange(1)} 
-        className={styles['pagination-button']}
-        disabled={currentPage === 1}
-      >
-        <i className="fas fa-angle-double-left"></i>
-      </button>,
-      <button 
-        key="prev" 
-        onClick={() => onPageChange(currentPage - 1)} 
-        className={styles['pagination-button']}
-        disabled={currentPage === 1}
-      >
-        <i className="fas fa-chevron-left"></i>
-      </button>
-    );
+//     pages.push(
+//       <button 
+//         key="first" 
+//         onClick={() => onPageChange(1)} 
+//         className={styles['pagination-button']}
+//         disabled={currentPage === 1}
+//       >
+//         <i className="fas fa-angle-double-left"></i>
+//       </button>,
+//       <button 
+//         key="prev" 
+//         onClick={() => onPageChange(currentPage - 1)} 
+//         className={styles['pagination-button']}
+//         disabled={currentPage === 1}
+//       >
+//         <i className="fas fa-chevron-left"></i>
+//       </button>
+//     );
 
-    if (startPage > 1) {
-      pages.push(
-        <button key={1} onClick={() => onPageChange(1)} className={styles['pagination-button']}>1</button>
-      );
-      if (startPage > 2) {
-        pages.push(<span key="dots1" className={styles['pagination-dots']}>...</span>);
-      }
-    }
+//     if (startPage > 1) {
+//       pages.push(
+//         <button key={1} onClick={() => onPageChange(1)} className={styles['pagination-button']}>1</button>
+//       );
+//       if (startPage > 2) {
+//         pages.push(<span key="dots1" className={styles['pagination-dots']}>...</span>);
+//       }
+//     }
 
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(
-        <button
-          key={i}
-          onClick={() => onPageChange(i)}
-          className={`${styles['pagination-button']} ${currentPage === i ? styles['pagination-button.active'] : ''}`}
-        >
-          {i}
-        </button>
-      );
-    }
+//     for (let i = startPage; i <= endPage; i++) {
+//       pages.push(
+//         <button
+//           key={i}
+//           onClick={() => onPageChange(i)}
+//           className={`${styles['pagination-button']} ${currentPage === i ? styles['pagination-button.active'] : ''}`}
+//         >
+//           {i}
+//         </button>
+//       );
+//     }
 
-    if (endPage < totalPages) {
-      if (endPage < totalPages - 1) {
-        pages.push(<span key="dots2" className={styles['pagination-dots']}>...</span>);
-      }
-      pages.push(
-        <button key={totalPages} onClick={() => onPageChange(totalPages)} className={styles['pagination-button']}>
-          {totalPages}
-        </button>
-      );
-    }
+//     if (endPage < totalPages) {
+//       if (endPage < totalPages - 1) {
+//         pages.push(<span key="dots2" className={styles['pagination-dots']}>...</span>);
+//       }
+//       pages.push(
+//         <button key={totalPages} onClick={() => onPageChange(totalPages)} className={styles['pagination-button']}>
+//           {totalPages}
+//         </button>
+//       );
+//     }
 
-    pages.push(
-      <button 
-        key="next" 
-        onClick={() => onPageChange(currentPage + 1)} 
-        className={styles['pagination-button']}
-        disabled={currentPage === totalPages}
-      >
-        <i className="fas fa-chevron-right"></i>
-      </button>,
-      <button 
-        key="last" 
-        onClick={() => onPageChange(totalPages)} 
-        className={styles['pagination-button']}
-        disabled={currentPage === totalPages}
-      >
-        <i className="fas fa-angle-double-right"></i>
-      </button>
-    );
+//     pages.push(
+//       <button 
+//         key="next" 
+//         onClick={() => onPageChange(currentPage + 1)} 
+//         className={styles['pagination-button']}
+//         disabled={currentPage === totalPages}
+//       >
+//         <i className="fas fa-chevron-right"></i>
+//       </button>,
+//       <button 
+//         key="last" 
+//         onClick={() => onPageChange(totalPages)} 
+//         className={styles['pagination-button']}
+//         disabled={currentPage === totalPages}
+//       >
+//         <i className="fas fa-angle-double-right"></i>
+//       </button>
+//     );
 
-    return pages;
-  };
+//     return pages;
+//   };
 
-  return <div className={styles['pagination']}>{renderPaginationButtons()}</div>;
-};
+//   return <div className={styles['pagination']}>{renderPaginationButtons()}</div>;
+// };
 
 
 function AdvancedSearch() {
